1caccde04ced30d65d9e6fc47ef0b9339c39ddc3
Pentatonix0 <Al0250443@yandex.ru>
2025-04-14 18:05:46 +0300

    Update

--- backend/application/db_models/models.py
+++ backend/application/db_models/models.py
@@ -21,7 +21,7 @@ class User(BaseModel):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
-    name = db.Column(db.String(564), unique=True, nullable=False)
+    name = db.Column(db.String(64), nullable=False)
     password = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
